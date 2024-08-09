4d456c6de24f1f6678eb34c36e7880d9f8ea64bf
Eng You Guan <youguan@gmail.com>
2024-08-09 20:58:02 +0800

    Fixed bug: Label not showing new tag name after user renames tag Force SwiftUI to watch for changes and explicitly read from tag's name where possible instead of from derived computed tagname

--- FeedbackAssistant/Filters/UserFilterRow.swift
+++ FeedbackAssistant/Filters/UserFilterRow.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct UserFilterRow: View {
+    @EnvironmentObject var dataController: DataController
+
     var filter: Filter
     var rename: (Filter) -> Void
     var delete: (Filter) -> Void
 
     var body: some View {
         NavigationLink(value: filter) {
-            Label(filter.name, systemImage: filter.icon)
+            Label(filter.tag?.name ?? "No name", systemImage: filter.icon)
                 .badge("\(filter.tag?.tagActiveIssues.count ?? 0) issues")
                 .contextMenu(ContextMenu(menuItems: {
                     Button {
